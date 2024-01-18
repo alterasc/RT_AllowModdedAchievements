46ead9c36be87632bc371c6234a601c9d4c30824
alterasc <alterasc@protonmail.com>
2024-01-18 12:09:46 +0100

    update for game version 1.0.97

--- AllowModdedAchievements/AchievementPatches.cs
+++ AllowModdedAchievements/AchievementPatches.cs
@@ -1,29 +1,16 @@
 ﻿using HarmonyLib;
 using Kingmaker;
-using Kingmaker.Modding;
 
 namespace AllowModdedAchievements;
-
-[HarmonyPatch(typeof(OwlcatModificationsManager), nameof(OwlcatModificationsManager.IsAnyModActive), MethodType.Getter)]
-public static class OwlcatModificationsManager_IsAnyModActive
-{
-    [HarmonyPrefix]
-    [HarmonyAfter("0ToyBox0")]
-    public static bool Prefix(ref bool __result)
-    {
-        __result = false;
-        return false;
-    }
-}
 
 [HarmonyPatch(typeof(Player), nameof(Player.ModsUser), MethodType.Getter)]
 public static class Player_ModsUser
 {
-    [HarmonyPrefix]
+    [HarmonyPostfix]
     [HarmonyAfter("0ToyBox0")]
-    public static bool Prefix(ref bool __result)
+    [HarmonyPriority(Priority.Last)]
+    public static void Postfix(ref bool __result)
     {
         __result = false;
-        return false;
     }
 }
